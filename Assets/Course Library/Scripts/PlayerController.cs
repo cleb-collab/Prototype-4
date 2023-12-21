66442d8fe0c5fb8e38d74333ba61fe4df7dc6970
cleb-collab <CLeberr@nycstudents.net>
2023-12-21 12:33:44 -0500

    Initial commit

---
+++ Assets/Course Library/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float speed = 5.0f;
+    private GameObject focalPoint;
+    private float fowardInput; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float fowardInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(focalPoint.transform.forward * speed * fowardInput);
+    }
+}
