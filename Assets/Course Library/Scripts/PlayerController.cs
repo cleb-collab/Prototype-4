c97ac389b003c78e08d04a13153be8763aa807d6
cleb-collab <CLeberr@nycstudents.net>
2024-01-08 12:41:06 -0500

    proto 4.3.7

--- Assets/Course Library/Scripts/PlayerController.cs
+++ Assets/Course Library/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     private GameObject focalPoint;
     private float fowardInput;
     public bool hasPowerup;
+    private float powerupStrength = 15.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,6 +28,21 @@ public class PlayerController : MonoBehaviour
         if (other.CompareTag("Powerup")) {
             hasPowerup = true;
             Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+
         }
+    }
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup) {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();   
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+
+            Debug.Log("Collide with" + collision.gameObject.name + "with powerup set to" + hasPowerup);
+            enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+        }
+    }
+    IEnumerator PowerupCountdownRoutine() {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
     }
 }
